c7821df9f00926bf97f42401d5b82e35c5fe0e2c
kamiada <kamiada528@gmail.com>
2021-08-31 08:16:32 +0100

    added text option

--- main.go
+++ main.go
@@ -11,11 +11,12 @@ import (
 	"fyne.io/fyne/v2/widget"
 )
 
-const memeURL = "http://apimeme.com/meme?meme=Advice-Doge&top=Much+sense&bottom=Much+love"
+
+const memeURL = "http://apimeme.com/meme?meme=Bad-Advice-Cat&"
 
 func main() {
 	myApp := app.New()
-	myWindow := myApp.NewWindow("Gopher")
+	myWindow := myApp.NewWindow("Meme generator")
 
 	//menu
 	fileMenu := fyne.NewMenu("File",
@@ -34,37 +35,46 @@ func main() {
 	myWindow.SetMainMenu(mainMenu)
 
 	//when  you create new var and you assign something to it, then you use :=
-	text := canvas.NewText("Hello world!", color.White)
+	text := canvas.NewText("Hello Meme-lover!", color.White)
 	//when you use feature, function of this variable and you assign value to it you use =
 	text.Alignment = fyne.TextAlignCenter
 
 	var resource, _ = fyne.LoadResourceFromURLString(memeURL)
 	gopherImage := canvas.NewImageFromResource(resource)
-	gopherImage.SetMinSize(fyne.Size{Width: 500, Height: 500}) 
+	gopherImage.SetMinSize(fyne.Size{Width: 500, Height: 500})
+
+	//input box
+	inputTop := widget.NewEntry()
+	inputTop.SetPlaceHolder("Type the top text for your meme here")
+
+	inputBottom := widget.NewEntry()
+	inputBottom.SetPlaceHolder("Type the bottom text for your meme here")
 
 	//making button
 	btn := widget.NewButton("Click me!", func() {
-		resource, _ := fyne.LoadResourceFromURLString(memeURL)
-        gopherImage.Resource = resource
+		resource, _ := fyne.LoadResourceFromURLString(memeURL + "top=" + inputTop.Text + "&bottom=" + inputBottom.Text)
+		gopherImage.Resource = resource
 		//Redrawn the image with the new path
-        gopherImage.Refresh()
+		gopherImage.Refresh()
 	})
 	btn.Importance = widget.HighImportance
+
 	box := container.NewVBox(
-		text, 
+		text,
 		gopherImage,
+		inputTop,
+		inputBottom,
 		btn,
 	)
-	    // Display our content
+	// Display our content
 	myWindow.SetContent(box)
-	
+
 	myWindow.Canvas().SetOnTypedKey(func(keyEvent *fyne.KeyEvent) {
-		if keyEvent.Name == fyne.KeyEscape{
+		if keyEvent.Name == fyne.KeyEscape {
 			myApp.Quit()
 		}
 	})
 
-
-	//this actually displays 
+	//this actually displays
 	myWindow.ShowAndRun()
 }
